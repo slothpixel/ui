0df7cf153ab4505760c9212447583c3c47f7c5c1
builder-247 <olaakkonen@gmail.com>
2018-10-10 18:27:17 +0300

    Added linting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+  },
+  "settings": {
+    "import/resolver": {
+      node: {
+        paths: [path.resolve(__dirname, './src')],
+      },
+    },
+  },
+  "extends": "airbnb",
+  "rules": {
+    "max-len": ["error", { "code": 180, "ignoreTemplateLiterals": true, "ignoreStrings": true }],
+    "no-mixed-operators": ["error", {
+      "allowSamePrecedence": true
+    }],
+    "no-shadow": 1,
+    "import/named": ["error"],
+    "jsx-a11y/anchor-is-valid": ["warn", {
+      "components": ["Link"],
+      "specialLink": ["to"],
+    }],
+    "react/no-array-index-key": 1,
+    "react/require-default-props": 0,
+    "react/sort-comp": [2]
+  },
+};
