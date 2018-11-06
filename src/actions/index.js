ef37a146f114a5078d0600651ba29b9bc86ac8c9
builder-247 <olaakkonen@gmail.com>
2018-11-06 16:11:26 +0200

    Changed api paths as per https://github.com/slothpixel/core/commit/84608b4e17ad9a313910ff98b35b8ec1772ff452

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import action from './action';
 import { langs } from '../lang';
 
-export const getPlayer = accountId => action('player', process.env.REACT_APP_API_HOST, `api/player/${accountId}`);
-export const getGuild = accountId => action('guild', process.env.REACT_APP_API_HOST, `api/guild/${accountId}`);
+export const getPlayer = accountId => action('player', process.env.REACT_APP_API_HOST, `api/players/${accountId}`);
+export const getGuild = accountId => action('guild', process.env.REACT_APP_API_HOST, `api/guilds/${accountId}`);
 export const getStrings = () => async (dispatch) => {
   const savedLang = window.localStorage && window.localStorage.getItem('localization');
   const defaultLang = langs[0];
