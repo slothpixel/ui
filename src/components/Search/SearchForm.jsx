e43909e93c89196cc4569a2e3b631a7ed75627e8
builder-247 <olaakkonen@gmail.com>
2018-12-13 15:41:10 +0200

    Top search bar now works

--- src/components/Search/SearchForm.jsx
+++ src/components/Search/SearchForm.jsx
@@ -1,16 +1,12 @@
-/* eslint-disable */
-// This file is WIP
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import TextField from 'material-ui/TextField';
-import querystring from 'querystring';
 import constants from '../constants';
 
 class SearchForm extends React.Component {
   static propTypes = {
-    dispatchSearch: PropTypes.func,
     location: PropTypes.shape({
       key: PropTypes.string,
     }),
@@ -23,37 +19,22 @@ class SearchForm extends React.Component {
 
   constructor(props) {
     super(props);
-
-    const params = querystring.parse(window.location.search.substring(1));
-    const { pathname } = window.location;
-    if (params.q && pathname === '/search') {
-      this.props.dispatchSearch(params.q);
-      this.state = {
-        query: params.q,
-      };
-    } else {
-      this.state = {};
-    }
+    this.state = {};
   }
 
   formSubmit = (e) => {
     const { query } = this.state;
+    const { history } = this.props;
     e.preventDefault();
-    this.props.history.push(`/search?q=${query}`);
-    this.props.dispatchSearch(query);
+    history.push(`/players/${query}`);
   };
 
   handleChange = (e) => {
-    const { pathname } = window.location;
     const { value } = e.target;
 
     this.setState({
       query: value,
     });
-
-    if (pathname === '/search') {
-      this.debouncedSetQuery(value);
-    }
   };
 
   render() {
